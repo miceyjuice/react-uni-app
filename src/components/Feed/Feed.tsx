9b310f6f521d54f7533ef5deda5db89df6649c6e
miceyjuice <ganjafz@gmail.com>
2021-05-10 16:33:47 +0200

    updated feed component

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,6 +1,11 @@
 import React, { FC } from "react";
 import styled from "styled-components";
 import Theme from "../../styledHelpers/Theme";
+import {
+  Search,
+  SearchIcon,
+  SearchInput,
+} from "../Entities/Options/OptionsStyle";
 
 const Wrapper = styled.div`
   display: flex;
@@ -15,14 +20,13 @@ const TopBar = styled.div`
   width: 100%;
 `;
 const Title = styled.h3`
-  color: ${Theme.Colors.white};
+  color: ${Theme.Colors.red};
 `;
 const Options = styled.div`
   display: flex;
+  gap: 2rem;
 `;
-const Filter = styled.div``;
-const FilterInput = styled.input``;
-const FilterImg = styled.img``;
+
 const Selection = styled.select``;
 const Option = styled.option``;
 
@@ -32,10 +36,10 @@ export const Feed: FC = () => {
       <TopBar>
         <Title>Resume your work</Title>
         <Options>
-          <Filter>
-            <FilterInput />
-            <FilterImg />
-          </Filter>
+          <Search>
+            <SearchInput placeholder="Search..." />
+            <SearchIcon src={process.env.PUBLIC_URL + Theme.Icons.search} />
+          </Search>
           <Selection>
             <Option>Followed</Option>
             <Option>All</Option>
