63b8965778d8aac34a4d2424cb01103f3ef8a44b
miceyjuice <ganjafz@gmail.com>
2021-05-11 16:35:10 +0200

    bbasic style for displaying comments

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -3,6 +3,7 @@ import { useSelector } from "react-redux";
 import styled from "styled-components";
 import { IState } from "../../reducers";
 import { IUsersCommentsReducer } from "../../reducers/usersCommentsReducers";
+import { IUsersReducer } from "../../reducers/usersReducers";
 import Theme from "../../styledHelpers/Theme";
 import {
   Search,
@@ -19,14 +20,47 @@ import {
   Option,
 } from "./FeedStyle";
 
-const WorkList = styled.div``;
+const WorkList = styled.div`
+  color: white;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
+
+const Work = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  border: 1px solid #444;
+  border-radius: 0.5rem;
+  padding: 1rem;
+  background-color: #222;
+`;
+const WorkTitle = styled.h3`
+  margin: 0.5rem 0;
+`;
+const Content = styled.p`
+  color: #ddd;
+  margin-bottom: 0.5rem;
+`;
+const BottomBar = styled.div`
+  display: flex;
+  gap: 1rem;
+  margin: 0;
+  font-size: ${Theme.FontSizes[14]};
+  color: #888;
+`;
+const CompanyName = styled.p``;
+const Category = styled.p``;
+const LastUpdate = styled.p``;
 
 export const Feed: FC = () => {
-  const { usersCommentsList } = useSelector<IState, IUsersCommentsReducer>(
-    (globalState) => ({
-      ...globalState.usersCommentsList,
-    })
-  );
+  const { usersCommentsList, usersList } = useSelector<
+    IState,
+    IUsersCommentsReducer & IUsersReducer
+  >((globalState) => ({
+    ...globalState.usersCommentsList,
+    ...globalState.users,
+  }));
 
   return (
     <Wrapper>
@@ -47,9 +81,18 @@ export const Feed: FC = () => {
       </TopBar>
       <WorkList>
         {usersCommentsList?.slice(0, 49).map((comment) => (
-          <div>
-            <p>{comment.name}</p>
-          </div>
+          <Work>
+            <WorkTitle>{comment.name}</WorkTitle>
+            <Content>{comment.body}</Content>
+            <BottomBar>
+              <CompanyName>Subsid. corp</CompanyName>
+              <Category>Contract</Category>
+              <LastUpdate>
+                Updated 3 days ago by{" "}
+                {usersList.find((user) => user.id === comment.postId)?.name}
+              </LastUpdate>
+            </BottomBar>
+          </Work>
         ))}
       </WorkList>
     </Wrapper>
