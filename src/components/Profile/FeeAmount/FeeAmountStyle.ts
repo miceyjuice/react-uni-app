35e21f7f1c5d6135da32ba0807cc1cb49c966938
miceyjuice <ganjafz@gmail.com>
2021-06-14 17:48:52 +0200

    moved array and styles to a seperate file

---
+++ src/components/Profile/FeeAmount/FeeAmountStyle.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+import { InfoBox } from "../Proposals/ProposalsStyle";
+
+export const FeeBox = styled(InfoBox)`
+  grid-template-columns: repeat(4, 1fr);
+`;
---
+++ src/components/Profile/FeeAmount/FeesArray.ts
@@ -0,0 +1,218 @@
+import { IReviews } from "../InternalReviews/InternalReviews";
+
+export const fees: IReviews[] = [
+    {
+      title: "Year",
+      fields: [
+        {
+          options: [
+            {
+              key: "year",
+              value: 1998,
+            },
+            {
+              key: "year",
+              value: 2005,
+            },
+            {
+              key: "year",
+              value: 2017,
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "year",
+              value: 2001,
+            },
+            {
+              key: "year",
+              value: 2005,
+            },
+            {
+              key: "year",
+              value: 2017,
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "year",
+              value: 2009,
+            },
+            {
+              key: "year",
+              value: 2005,
+            },
+            {
+              key: "year",
+              value: 2010,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Cost center",
+      fields: [
+        {
+          options: [
+            {
+              key: "cost",
+              value: "CS 153",
+            },
+            {
+              key: "cost",
+              value: "CS 153",
+            },
+            {
+              key: "cost",
+              value: "CS 47",
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "cost",
+              value: "CS 47",
+            },
+            {
+              key: "cost",
+              value: "CS 153",
+            },
+            {
+              key: "cost",
+              value: "CS 47",
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "cost",
+              value: "CS 188",
+            },
+            {
+              key: "cost",
+              value: "CS 153",
+            },
+            {
+              key: "cost",
+              value: "CS 47",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Total amount",
+      fields: [
+        {
+          options: [
+            {
+              key: "totalAmount",
+              value: "3500 €",
+            },
+            {
+              key: "totalAmount",
+              value: "9500 €",
+            },
+            {
+              key: "totalAmount",
+              value: "10500 €",
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "totalAmount",
+              value: "17000 €",
+            },
+            {
+              key: "totalAmount",
+              value: "9500 €",
+            },
+            {
+              key: "totalAmount",
+              value: "10500 €",
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "totalAmount",
+              value: "20500 €",
+            },
+            {
+              key: "totalAmount",
+              value: "9500 €",
+            },
+            {
+              key: "totalAmount",
+              value: "10500 €",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      title: "Law firm",
+      fields: [
+        {
+          options: [
+            {
+              key: "lawFirm",
+              value: "Clifford chance",
+            },
+            {
+              key: "lawFirm",
+              value: "Linklaters",
+            },
+            {
+              key: "lawFirm",
+              value: "Bigmatters",
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "lawFirm",
+              value: "Linklaters",
+            },
+            {
+              key: "lawFirm",
+              value: "Bigmatters",
+            },
+            {
+              key: "lawFirm",
+              value: "Clifford chance",
+            },
+          ],
+        },
+        {
+          options: [
+            {
+              key: "lawFirm",
+              value: "Bigmatters",
+            },
+            {
+              key: "lawFirm",
+              value: "Linklaters",
+            },
+            {
+              key: "lawFirm",
+              value: "Clifford chance",
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  export default fees;
