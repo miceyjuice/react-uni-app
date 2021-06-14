185dca2b99d50c0fe40372d25c1f41cae13ff899
miceyjuice <ganjafz@gmail.com>
2021-06-14 21:55:05 +0200

    separated filter component's styles

--- src/components/Navbar/ExpandedUl/Filter/Filter.tsx
+++ src/components/Navbar/ExpandedUl/Filter/Filter.tsx
@@ -1,14 +1,6 @@
 import { FC, useState, ChangeEvent, Dispatch, SetStateAction } from "react";
-import styled from "styled-components";
 import Li from "../../../../styledHelpers/components/navbar/Li";
-import SearchInput from "../../../../styledHelpers/components/navbar/SearchInput";
-
-const FilterSearchInput = styled(SearchInput)`
-  max-height: 1.875rem;
-  padding: 1.2rem;
-  width: 100% !important;
-  font-size: 0.9rem;
-`;
+import { FilterSearchInput } from "./FilterStyle";
 
 interface IFilter {
   setFilterValue: Dispatch<SetStateAction<string>>;
---
+++ src/components/Navbar/ExpandedUl/Filter/FilterStyle.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import SearchInput from "../../../../styledHelpers/components/navbar/SearchInput";
+
+export const FilterSearchInput = styled(SearchInput)`
+  max-height: 1.875rem;
+  padding: 1.2rem;
+  width: 100% !important;
+  font-size: 0.9rem;
+`;
